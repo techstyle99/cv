dcd6900033ce4558689d3749d32631ec93da82dd
George August <techstyle99@gmail.com>
2021-04-20 16:08:14 -0400

    added initial CV website files

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// This is a JavaScript page
+/* Javascript can be placed in the <body>
+   or <head> section or both internally in 
+   an HTML page or it can be 
+   placed externally */
+// placing script at the bottom of the <body> element improve the display speed
+
+
+function myFunction() {
+// document.getElementById("demo").innerHTML = "Hello JavaScript!";
+document.getElementById("demo").src='pic_bulbon.gif';
+}
+
